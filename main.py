7d633bc0c978207dbf58aa0edb80adad70bbfc76
Catalin Lucaciu <catalinlucaciu13@gmail.com>
2023-10-22 13:54:30 +0300

    Create main.py
    added lab 2

---
+++ main.py
@@ -0,0 +1,51 @@
+class HashTable:
+    def __init__(self, size=1000):
+        self.size = size
+        self.table = [[] for _ in range(self.size)]
+    
+    def hash(self, key):
+        return hash(key) % self.size
+    
+    def insert(self, key, value):
+        index = self.hash(key)
+        for item in self.table[index]:
+            if item[0] == key:
+                item[1] = value  
+                return
+        self.table[index].append([key, value])
+    
+    def get(self, key):
+        index = self.hash(key)
+        for kv in self.table[index]:
+            if kv[0] == key:
+                return kv[1]
+        return None 
+    
+    def delete(self, key):
+        index = self.hash(key)
+        for i, item in enumerate(self.table[index]):
+            if item[0] == key:
+                self.table[index].pop(i)
+                return
+
+class SymbolTable:
+    def __init__(self):
+        self.identifiers = HashTable()
+    
+    def insert(self, key, value):
+        self.identifiers.insert(key, value)
+    
+    def get(self, key):
+        return self.identifiers.get(key)
+    
+    def delete(self, key):
+        self.identifiers.delete(key)
+
+st_identifiers = SymbolTable()
+st_constants = SymbolTable()
+
+st_identifiers.insert("a", 10)
+st_constants.insert("b", 21)
+
+print(st_identifiers.get("a"))  
+print(st_constants.get("b")) 
